ec0da62aefea655722ae3dcfde0aa5aad6b3d99d
Leandro López (inkel) <inkel.ar@gmail.com>
2012-11-19 15:08:49 -0300

    Use Emacs as my Git editor
    I'm using this configuration in my ~/.gitconfig
    
        git config --global core.editor "/usr/local/bin/emacs -q -l ~/.emacs.d/giteditor.el -nw"
    
    See https://github.com/lunaryorn/git-modes for more information

---
+++ giteditor.el
@@ -0,0 +1,11 @@
+;; My emacs configuration to use it as my Git editor
+
+;; I'm using this configuration in my ~/.gitconfig
+
+;; git config --global core.editor "/usr/local/bin/emacs -q -l ~/.emacs.d/giteditor.el -nw"
+
+;; See https://github.com/lunaryorn/git-modes
+
+(require 'package)
+(package-initialize)
+(require 'git-commit-mode)
