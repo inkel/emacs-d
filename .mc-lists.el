d3a39fc3f150c31165032b0281a42f9b083f390b
Leandro López (inkel) <inkel.ar@gmail.com>
2014-12-24 08:52:26 -0300

    Multiple cursors automatic configuration

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,11 +3,23 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        c-electric-lt-gt
+        coffee-dedent-line-backspace
+        downcase-region
+        enh-ruby-electric-brace
+        go-mode-insert-and-indent
         ruby-electric-curlies
         ruby-electric-space
+        upcase-region
         ))
 
 (setq mc/cmds-to-run-once
       '(
+        beginning-of-buffer
+        end-of-buffer
+        kill-region
+        magit-status
+        markdown-exdent-or-delete
+        org-self-insert-command
         ruby-electric-matching-char
         ))
