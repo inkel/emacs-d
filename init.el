420770fd6608d50d716722ecf03ce06bc4a0bd59
Leandro López <inkel.ar@gmail.com>
2020-07-01 15:07:05 -0300

    Add basic settings

--- init.el
+++ init.el
@@ -41,6 +41,37 @@
   ;; Loading compiled files
   (customize-set-variable 'load-prefer-newer t)
 
+  ;; Misc settings
+
+  ;;; Paste where cursor is
+  (customize-set-variable 'mouse-yank-at-point t)
+
+  ;;; Show line numbers
+  (use-package display-line-numbers
+    :defer nil
+    :ensure nil
+    :config (global-display-line-numbers-mode))
+
+  ;;; Trailing whitespace is bad and you should feel bad
+  (customize-set-variable 'show-trailing-whitespace t)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+  ;;; Matching parenthesis
+  (show-paren-mode)
+
+  ;;; Fuck tabs!
+  (customize-set-variable 'indent-tabs-mode nil)
+
+  ;;; Those pesky backups files…
+  (customize-set-variable 'backup-directory-alist
+                          `(("." . ,(concat user-emacs-directory "backups"))))
+
+  ;; Adds a new line at the last line
+  (setq require-final-newline t)
+
+  ;;; Sound off
+  (setq ring-bell-function 'ignore)
+
   ;; Set GC threshold to 1GB
   (setq gc-cons-threshold (* 1000 1000))
   ) ;; End prevent of special filename parsing
