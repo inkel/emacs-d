6a21c8090352a410ecc64512a44e30b9712fbac9
Leandro López <inkel.ar@gmail.com>
2020-07-01 14:56:31 -0300

    Use use-package for package management

--- init.el
+++ init.el
@@ -18,7 +18,28 @@
   (if (file-exists-p custom-file)
       (load custom-file))
 
+  ;; Package management
+  (customize-set-variable 'package-archives
+                          '(("gnu"       . "https://elpa.gnu.org/packages/")
+                            ("marmalade" . "https://marmalade-repo.org/packages/")
+                            ("melpa"     . "https://melpa.org/packages/")))
 
+  (package-initialize)
+
+  (when (not package-archive-contents)
+    (package-refresh-contents))
+
+  (when (not (package-installed-p 'use-package))
+    (package-install 'use-package))
+
+  (require 'use-package)
+
+  (customize-set-variable 'use-package-always-ensure t)
+  (customize-set-variable 'use-package-always-defer t)
+  (customize-set-variable 'use-package-verbose nil)
+
+  ;; Loading compiled files
+  (customize-set-variable 'load-prefer-newer t)
 
   ;; Set GC threshold to 1GB
   (setq gc-cons-threshold (* 1000 1000))
