558ea57168420417ea3dcea20d643919018f7986
Leandro López (inkel) <inkel.ar@gmail.com>
2020-12-01 07:22:05 -0300

    Remove marmalade-repo.org from package sources list
    This URL has been giving an SSL error for a long time already.

--- init.el
+++ init.el
@@ -24,7 +24,6 @@
   ;; Package management
   (customize-set-variable 'package-archives
                           '(("gnu"       . "https://elpa.gnu.org/packages/")
-                            ("marmalade" . "https://marmalade-repo.org/packages/")
                             ("melpa"     . "https://melpa.org/packages/")))
 
   (package-initialize)
