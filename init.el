dd908e0bb742adbaf08af3f2e419f34a44cd42e6
Leandro López (inkel) <inkel.ar@gmail.com>
2021-04-07 14:53:26 -0300

    Fix lsp-mode bindings
    Signed-off-by: Leandro López (inkel) <inkel.ar@gmail.com>

--- init.el
+++ init.el
@@ -281,10 +281,12 @@
     :hook ((before-save . lsp-format-buffer)
            (before-save . lsp-organize-imports)
            (go-mode . lsp-deferred))
-    :bind (("C-c e d" . lsp-find-implementation)
-           ("C-c e t" . lsp-find-type-definition)
+    :bind (("C-c e d" . lsp-find-definition)
            ("C-c e r" . lsp-find-references)
            ("C-c e R" . lsp-rename)))
+
+  (with-eval-after-load 'lsp-mode
+    (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
 
   ;; Go programming
   (setenv "GOPATH" "/Users/inkel/dev/go")
